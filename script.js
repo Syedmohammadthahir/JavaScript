c1ec3317aee3d57c6d5eb863acd54078a9242c5a
Syed Thahir <34040173+Syedmohammadthahir@users.noreply.github.com>
2018-11-03 22:30:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Welcome to Creative Connect");
+
+document.write("Welcome to JavaScript First Class");
+
+alert("Thanks for Visiting!!")
---
+++ script2.js
@@ -0,0 +1,13 @@
+// alert("Welcome to Creative Connect");
+
+// document.write("This is my First JavaScript program");
+
+// alert("Thanks for Visiting, Please visit again!!!!")
+
+
+
+var firstName = "Creative";
+var lastName = "Connect";
+
+
+console.log(firstName + " " + lastName);
